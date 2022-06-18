2169b0f2b4b7b6adf5f0dfc72c119ed442ac9164
Samuel John <opeoniye@gmai.com>
2022-06-18 23:41:45 +0100

    initial commit

---
+++ assets/js/clock.js
@@ -0,0 +1,24 @@
+// countdown to the wedding
+function updateTimer() {
+    future = Date.parse("aug 13, 2022 01:30:00");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("clock")
+        .innerHTML =
+        '<div>' + d + '<span>Days</span></div>' +
+        '<div>' + h + '<span>Hours</span></div>' +
+        '<div>' + m + '<span>Minutes</span></div>' +
+        '<div>' + s + '<span>Seconds</span></div>';
+}
+setInterval('updateTimer()', 1000);
